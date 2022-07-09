34fae38ce1c242e6e9058766e02336ffe220d458
katayama <tattu.0310@gmail.com>
2022-07-09 14:53:26 +0900

    🎉 top10

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "src/lib/mantine";
 import axios from "axios";
 import { useState } from "react";
 
-type coconalaDemand = {
+type coconalaType = {
   avg: number;
   category: string;
   count: number;
@@ -17,17 +17,41 @@ type coconalaDemand = {
   sum: number;
 };
 const Home: NextPage = () => {
-  const [demand, setdemand] = useState<coconalaDemand[]>();
   const handleClick = async () => {
     const coconalaRank = await axios.get(
       "https://coconala-requests-checker.vercel.app/1f63a122/rank/"
     );
-    console.log(coconalaRank.data.status);
-    if (coconalaRank.data.status === 200) {
-      console.log(coconalaRank.data.items.filter((v) => v.month == "2022-07"));
 
-      setdemand(coconalaRank.data.items.filter((v) => v.month == "2022-07"));
+    if (coconalaRank.data.status === 200) {
+      //すべてのデータ
+      const AllData = coconalaRank.data.items;
+      getRankTop10(AllData);
     }
+  };
+
+  const getRankTop10 = (coconalaRank: coconalaType[]) => {
+    //ひと月のデータ
+    const data = coconalaRank.filter(
+      (v: { month: string }) => v.month == "2022-07"
+    );
+    //{ category: v.category, count: 0 }の配列を作成
+    let array: { category: string; count: number }[] = [];
+    data.map((v: { category: string }) => {
+      array.push({ category: v.category, count: 0 });
+    });
+    //categoryが同じならcountを加算
+    array.forEach((v) => {
+      coconalaRank?.forEach((v2: { category: string; count: number }) => {
+        if (v.category === v2.category) {
+          v.count += v2.count;
+        }
+      });
+    });
+    //オブジェクトの昇順ソート
+    let result = array.sort((a, b) => {
+      return a.count > b.count ? -1 : 1;
+    });
+    console.log(result);
   };
 
   return (
@@ -35,11 +59,6 @@ const Home: NextPage = () => {
       <Button dent onClick={handleClick} className="mt-4 block">
         Click me!
       </Button>
-      <div>
-        {demand?.map((item, index) => {
-          return <div key={index}>{item.category}</div>;
-        })}
-      </div>
     </div>
   );
 };
